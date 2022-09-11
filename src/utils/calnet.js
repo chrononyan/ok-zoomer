89f3ad4d12a9bd1440ddad14f1e266e7f457c7bb
Jerry Xu <jerryxu@berkeley.edu>
2022-09-11 20:03:59 +0000

    dbg

--- src/utils/calnet.js
+++ src/utils/calnet.js
@@ -3,6 +3,8 @@ import { URI as OTPURI } from "otpauth";
 import * as browserUtils from "./browser.js";
 import { loadConfig, saveConfig } from "../config.js";
 
+import sleep from "./sleep.js"
+
 export function generateDuoToken(opts = {}) {
   const { configPath } = opts;
   let { config } = opts;
@@ -58,15 +60,15 @@ export async function gotoWithAuth(page, url, opts = {}) {
     await passwordElem.type(password);
     await passwordElem.press("Enter");
 
-    const frameOrErrorElem = await page.waitForSelector(
-      "#loginForm #status, #duo_iframe > iframe",
+    const authOrErrorElem = await page.waitForSelector(
+      "#loginForm #status, #auth-view-wrapper .other-options-link > a",
       { visible: true }
     );
-    let isError = await frameOrErrorElem.evaluate(
-      (elem) => elem.tagName !== "IFRAME"
+    let isError = await authOrErrorElem.evaluate(
+      (elem) => elem.id === "status"
     );
     if (isError) {
-      const errorText = await frameOrErrorElem.evaluate((elem) =>
+      const errorText = await authOrErrorElem.evaluate((elem) =>
         elem.textContent.trim()
       );
       throw new Error(`CalNet login error: ${errorText}`);
@@ -78,57 +80,51 @@ export async function gotoWithAuth(page, url, opts = {}) {
     } else {
       console.error("[calnet] entering 2FA code");
 
-      const duoFrame = await page.waitForFrame(async (frame) => {
-        const url = frame.url();
-        if (!url) return false;
-        const urlObj = new URL(url);
-        if (!urlObj.hostname.endsWith(".duosecurity.com")) return false;
-        if (!urlObj.pathname.startsWith("/frame/prompt")) return false;
-        return true;
-      });
+      await authOrErrorElem.evaluate((elem) => elem.click());
+    await sleep(3000000)
 
       const duoDeviceName = config.get("calnet.duo.deviceName");
       if (!duoDeviceName)
         throw new Error(`Missing config option: calnet.duo.deviceName`);
 
-      const deviceSelectElem = await duoFrame.waitForSelector(
-        "#login-form .device-selector select",
+      const authMethodListElem = await page.waitForSelector(
+        ".all-auth-methods .other-options-list",
         { visible: true }
       );
-      const devices = {};
-      for (const deviceOptionElem of await deviceSelectElem.$$("option")) {
-        const label = await deviceOptionElem.evaluate(
-          (elem) => elem.textContent
+      const authMethods = [];
+      let foundAuthMethod = false;
+      for (const authMethodElem of await authMethodListElem.$$("li > a")) {
+        const authMethodLabel = await authMethodElem.evaluate(
+          (elem) => elem.querySelector(".method-label").textContent
         );
-        const deviceID = await deviceOptionElem.evaluate((elem) => elem.value);
-        const match = label.match(/^(.+?)(?: \(([^)]+)\))?$/);
-        const deviceName = match[1];
-        devices[deviceName] = deviceID;
+        console.log("label", authMethodLabel)
+        if (authMethodLabel === "Duo Mobile passcode") {
+          foundAuthMethod = true;
+          let promptURL = await authMethodElem.evaluate((elem) => elem.href);
+          console.log(promptURL)
+          await page.goto(promptURL);
+          break;
+        }
+        const authMethodDescription = await authMethodElem.evaluate(
+          (elem) => elem.querySelector(".method-description").textContent.trim()
+        );
+        authMethods.push(`${authMethodLabel}: ${authMethodDescription}`);
       }
-      const deviceID = devices[duoDeviceName];
-      if (!deviceID) {
+      if (!foundAuthMethod) {
         throw new Error(
-          `No Duo device named "${duoDeviceName}" found (available devices: [${Object.keys(
-            devices
-          ).join(", ")}])`
+          `No option for mobile passcode (available auth methods: [${authMethods.join(", ")}])`
         );
       }
-      await deviceSelectElem.select(deviceID);
-
-      const passcodeBtnElem = await duoFrame.waitForSelector(
-        `#auth_methods fieldset[data-device-index='${deviceID}'] button#passcode`,
-        { visible: true }
-      );
-      await passcodeBtnElem.evaluate((elem) => elem.click());
 
       token = generateDuoToken({ ...opts, config });
-      const passcodeInputElem = await duoFrame.waitForSelector(
-        `#auth_methods fieldset[data-device-index='${deviceID}'] input[name='passcode']`,
+      const passcodeInputElem = await page.waitForSelector(
+        `#auth-view-wrapper #passcode-input`,
         { visible: true }
       );
       await passcodeInputElem.type(token);
       await passcodeInputElem.press("Enter");
     }
+
 
     const maybeMessageElem = await page.waitForFunction(
       () => {
